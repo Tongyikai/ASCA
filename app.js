50ae20a6fb472ae96499b2f266942991fc04b03d
tongyikai <tongyikai@gmail.com>
2020-06-21 05:23:16 +0800

    新專案

---
+++ app.js
@@ -0,0 +1,102 @@
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+var querystring = require("querystring");
+
+var emailContr = require("./public/scripts/emailController");
+
+http.createServer(function(request, response) {
+	var params = url.parse(request.url, true).query;//parse將字符串轉成對象，request.url = "/?email=tong"， true表示params是{ email:"tong" }， false表示params是 email=tong
+	var action = url.parse(request.url, true).pathname;
+	var post = "";
+	
+	console.log("Request URL: " + request.url);//帶有參數的GET會顯示出來
+	console.log("action: " + action);//帶有參數的GET不會顯示
+
+	/* ************************************************
+	*	POST
+	************************************************ */
+	request.on("data", function(chunk) {
+		post += chunk;
+	});
+
+	request.on("end", function() {
+		if (request.url === "/userRegister") {
+			console.log("收到一個 POST 請求 " + post);
+			
+			post = querystring.parse(post);
+			console.log(post.familyName);
+			console.log(post.givenName);
+			console.log(post.email);
+			console.log(post.password);
+			console.log(post.bYear);
+			console.log(post.bMonth);
+			console.log(post.bDay);
+			console.log(post.gender);
+		}
+	});
+
+	/* ************************************************
+	*	URL 
+	************************************************ */
+	if (request.url === "/index") {
+		sendFileContent(response, "index.html", "text/html");
+
+	} else if (request.url === "/") {
+        sendFileContent(response, "index.html", "text/html");
+
+	} else if (request.url === "/signUp") {
+		sendFileContent(response, "signUp.html", "text/html");
+
+	} else if (action === "/signUp/check") { //action 只要判斷網域後面的URL，後面帶參數不需要考慮
+		checkEmail(response, params.email);
+		console.log("/signUp/check GET Request: " + params.email);
+
+	} else if (request.url === "/userRegister") {
+		console.log("Method:POST, Request: /userRegister " + post);
+
+	} else if (/^\/[a-zA-Z0-9\/]*.js$/.test(request.url.toString())) {
+		sendFileContent(response, request.url.toString().substring(1), "text/javascript");
+		console.log("Need File: Request --> " + request.url.toString().substring(1) + " .js");
+
+	} else if (/^\/[a-zA-Z0-9\/]*.css$/.test(request.url.toString())) {
+		sendFileContent(response, request.url.toString().substring(1), "text/css");
+		console.log("Need File: Request --> " + request.url.toString().substring(1) + ".css");
+
+	} else {
+		console.log("找不到對應的 -- Requested URL is: " + request.url);
+		response.end();
+	}
+	
+}).listen(8888);
+console.log("Server running at: http://127.0.0.1:8888/ \nNow time: " + new Date());
+
+/* ************************************************
+ * Method
+ ************************************************* */
+function sendFileContent(response, fileName, contentType) {
+	fs.readFile(fileName, function(err, data) {
+		if(err) {
+			response.writeHead(404);
+			response.write("Not Found!");
+
+		} else {
+			response.writeHead(200, {"Content-Type": contentType});
+			response.write(data);
+
+		}
+		response.end();
+	});
+}
+
+function checkEmail(response, email) {
+	if (emailContr.searchExistEmail(email)) { 
+		response.writeHead(200, { "Content-Type": "application/json" });
+		response.write(JSON.stringify({ emailAvailable: "false" }));//如果電子郵件已經存在，就給 false 不能使用 
+
+	} else {
+		response.writeHead(200, { "Content-Type": "application/json" });
+		response.write(JSON.stringify({ emailAvailable: "true" }));
+	}
+	response.end();
+}
---
+++ public/scripts/checkUserInfor.js
@@ -0,0 +1,101 @@
+
+nameRule = /[^\u4e00-\u9fa5]/;//只能輸入中文
+emailRule = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/;
+blankRule = /(^s*)|(s*$)/;
+passwordRule = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[\w\s]).{8,16}$/;
+
+//檢查是否輸入了空白
+function checkBlank(input) {
+    if (input.replace(/(^s*)|(s*$)/g, "").length == 0) {
+        return true;
+    }
+
+    if (input == "") return true;
+    var regulation = "^[ ]+$";
+    var re = new RegExp(regulation);
+    return re.test(input);
+}
+
+//檢查姓氏與名字長度
+function checkNameLength(name) {
+    if (name.length > 5) return false;
+    return true;
+}
+
+//檢查命名是否為中文
+function checkNameIsChinese(name) {
+    if (nameRule.test(name)) return false;
+    return true;
+}
+
+//檢查電子郵件格式是否正確
+function checkEmail(email) {
+    if (email.search(emailRule)!= -1) return true;
+        return false;
+}
+
+//AJAX
+var httpRequest = new XMLHttpRequest();
+var isAvailable;
+
+httpRequest.onload = function() {
+    if (httpRequest.status >= 200 && httpRequest.status < 400) {
+        var jsonObject = JSON.parse(httpRequest.responseText);
+
+        console.log("處理回應: " + httpRequest.responseText);
+        console.log("JSON: " + jsonObject);
+        console.log("emailAvailable is: " + jsonObject.emailAvailable);
+
+        if (jsonObject.emailAvailable == "true") {
+            isAvailable = true;
+        } else {
+            isAvailable = false;
+        }
+    }
+}
+
+httpRequest.onerror = function() {
+    alert("Can't connect to this network.");
+}
+// document.write('<script src="../HTTPServer/emailController.js"></script>');//引用另一個js
+
+//檢查電子郵件是否存在(避免註冊重複的電子郵件)
+function checkEmailAvailable(searchEmail) {
+    httpRequest.open("GET", "http://127.0.0.1:8888/signUp/check?email=" + searchEmail, false);
+    httpRequest.send();
+
+    return isAvailable;
+}
+
+//檢查密碼必須包含至少 8 個字元，可以混合使用英文字母、數字和符號 (僅限 ASCII 標準字元)
+function checkPassword(password) {
+    return passwordRule.test(password);
+}
+
+//檢查出生年月日是否正確
+function checkDateOfBirth(year, month, day) {
+    var limitInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    var isLeap = new Date(year, 1, 29).getDate() === 29;
+
+    if (isLeap) {
+        limitInMonth[1] = 29;
+    }
+
+    return day <= limitInMonth[month - 1];
+}
+
+//傳送使用者註冊的基本資料
+function registerForUser(familyName, givenName, email, password, bYear, bMonth, bDay, gender) {
+    // console.log("客戶端註冊資訊準備用POST傳給伺服器");
+    httpRequest.open("POST", "http://127.0.0.1:8888/userRegister", true);
+    httpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    httpRequest.send("familyName=" + familyName + 
+                     "&givenName=" + givenName +
+                     "&email=" + email +
+                     "&password=" + password +
+                     "&bYear=" + bYear + 
+                     "&bMonth=" + bMonth +
+                     "&bDay=" + bDay +
+                     "&gender=" + gender
+                     );
+}
